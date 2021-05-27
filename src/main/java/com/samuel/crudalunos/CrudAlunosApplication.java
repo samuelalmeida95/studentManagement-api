a29c728579f00e5b7607b91a38094c647f591863
samuelalmeida95 <samuelalmeida95>
2021-05-26 21:32:00 -0300

    base de dados automática da aplicação

--- src/main/java/com/samuel/crudalunos/CrudAlunosApplication.java
+++ src/main/java/com/samuel/crudalunos/CrudAlunosApplication.java
@@ -31,16 +31,13 @@ public class CrudAlunosApplication implements CommandLineRunner {
 		Aluno a1 = new Aluno(null, "Samuel Almeida", "1234");
 		Aluno a2 = new Aluno(null, "Gabrielle Barbosa", "010203");
 
-
 		alunoRepository.saveAll(Arrays.asList(a1, a2));
 
 		Professor p1 = new Professor(null, "Loiane", "20210156", "java15");
 		Professor p2 = new Professor(null, "Carlos", "20010186", "cobol+");
 		Professor p3 = new Professor(null, "Mayke", "20180266", "Js2020");
 
-		professorRepository.saveAll(Arrays.asList(p1, p2,p3));
-
-
+		professorRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
