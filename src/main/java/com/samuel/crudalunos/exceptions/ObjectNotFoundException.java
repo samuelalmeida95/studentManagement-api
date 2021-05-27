27529fd9185d820c0251b9705771cb2500cf0ad7
samuelalmeida95 <samuelalmeida95>
2021-05-26 21:40:23 -0300

    adicionando exception ( ObjetcNotFoundException ) para ser usada nas requisições não encontradas status 404

--- src/main/java/com/samuel/crudalunos/exceptions/ObjectNotFoundException.java
+++ src/main/java/com/samuel/crudalunos/exceptions/ObjectNotFoundException.java
@@ -10,8 +10,5 @@ public class ObjectNotFoundException extends RuntimeException {
 
     public ObjectNotFoundException(String message, Throwable cause) {
         super(message, cause);
-    }
-
-    
-    
+    } 
 }
